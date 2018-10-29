b87c17d6ac9e5e4397fd3178217691445060321e
shaojie <1104098541@qq.com>
2018-10-29 18:06:22 +0800

    增加并发代码

--- src/com/shaojie/bfbcys/ConcurrencyTest.java
+++ src/com/shaojie/bfbcys/ConcurrencyTest.java
@@ -1,4 +1,46 @@
 package com.shaojie.bfbcys;
 
+/**
+ * 在CPU是多核的情况下，当运算次数超过1亿后，concurrency要比serial快一倍,
+ * 主要原因是当运算次数增加后，线程的上下文切换的时长相对于运算的时长微乎其微
+ */
 public class ConcurrencyTest {
+    private static final long count = 80000000l;
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency :" + time+"ms,b="+b);
+    }
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial:" + time+"ms,b="+b+",a="+a);
+    }
 }
--- src/com/shaojie/demo/concurrent/Test.java
+++ src/com/shaojie/demo/concurrent/Test.java
@@ -1,4 +1,42 @@
 package com.shaojie.demo.concurrent;
 
+import java.lang.reflect.Field;
+import sun.misc.Unsafe;
+import sun.reflect.Reflection;
+
+class User {
+    private String name = "";
+    private int age = 0;
+
+    public User() {
+        this.name = "test";
+        this.age = 22;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + age;
+    }
+}
+
 public class Test {
+    public static void main(String[] args) throws NoSuchFieldException,
+            SecurityException, IllegalArgumentException, IllegalAccessException {
+        // 通过反射得到theUnsafe对应的Field对象
+        Unsafe.getUnsafe();
+        Field field = Unsafe.class.getDeclaredField("theUnsafe");
+        // 设置该Field为可访问
+        field.setAccessible(true);
+        // 通过Field得到该Field对应的具体对象，传入null是因为该Field为static的
+        Unsafe unsafe = (Unsafe) field.get(null);
+        System.out.println(unsafe);
+        User user = null;
+        try {
+            user = (User)unsafe.allocateInstance(User.class);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        System.out.println(user);
+
+    }
 }
